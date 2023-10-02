892b93644d0aaeb31c36803d5ef1144ac6e829a7
Aswathy Radha <aswathy.radha@gmail.com>
2023-10-02 09:15:32 -0500

    Prepare Functions

--- prepare.py
+++ prepare.py
@@ -12,87 +12,225 @@
 #     name: python3
 # ---
 
+# +
+# Import the Pandas library for data manipulation
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# Import the NumPy library for numerical operations
 import numpy as np
 
+#Import visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def prepare_store(df):
-    """
-    Prepares a sales DataFrame for analysis by:
-    1. Converting sale dates to datetime format.
-    2. Setting the datetime as the index.
-    3. Extracting the month and day of the week.
-    4. Calculating the total sales for each transaction.
+# Import custom library 
+import acquire
+# Import the 'os' library for operating system-related tasks
+import os
 
-    Args:
-        df (DataFrame): The input sales DataFrame.
 
-    Returns:
-        DataFrame: The prepared DataFrame.
-    """
-    # Convert the sale date into a datetime
-    df.sale_date = pd.to_datetime(df.sale_date, format='%Y-%m-%d')
-    #df.sale_date = pd.to_datetime(df.sale_date, format='%a, %d %b %Y')
-    # Set the index as the new datetime value
-    df = df.set_index('sale_date')
-    # Extract the month value (name of month)
-    df['month'] = df.index.strftime('%B')
-    # Extract the day of the week from the date
-    df['day of the week'] = df.index.day_name()
-    # Calculate the total sales
+#datetime utilities
+from datetime import timedelta, datetime
+
+
+# +
+################################### Prep Store Function #################################
+
+def prep_store_data():
+    '''
+    This function takes in a df and changes date dtypes, resets date as index,
+    creates new columns for month, weekday, and total sales
+    and returns that as a new pandas dataframe
+    '''
+
+    #assign variable df from the acquire function
+    df= acquire.get_store_data()
+
+    #change data type on sale_date
+    df.sale_date = df.sale_date.astype('datetime64[ns]')
+    #set the index to sale_date
+    df = df.set_index('sale_date').sort_index()
+
+    #create new colum for month
+    df['month'] = df.index.month_name()
+    #create new colum for weekday
+    df['day_of_week'] = df.index.day_name()
+    #create new colum for sale total
     df['sales_total'] = df.sale_amount * df.item_price
+
     return df
 
 
-def prep_germany_data(df):
+
+# +
+########################## Prep Germany Function ##########################
+
+def prep_germany_data():
     '''
-    Takes in a dataframe, coverts Date to DateTime format and makes it the index, produces histograms 
-    of all variables, renames "Wind+Solar" to the more python-friendly "WindSolar", fills the abundant 
-    nulls/NaNs with 0, and adds the columns 'month' and 'year'. We take a look at the amended dataframe
-    and the dataframe is returned.
+    This function takes in a df and changes date dtypes, resets date as index,
+    creates new columns for month, weekday, and total sales
+    and returns that as a new pandas dataframe
     '''
-    # Convert date to datetime for time series analysis
-    df.Date = pd.to_datetime(df.Date)
+
+    #assign variable df from the acquire function
+    df= acquire.get_germany_data()
+
+    #change data type on Date
+    df.Date = df.Date.astype('datetime64[ns]')
+    #set the index to Date
+    df = df.set_index('Date').sort_index()
+    #rename columns
+    df = df.rename(columns={'Consumption':'consumption', 'Wind':'wind', 'Solar':'solar','Wind+Solar':'wind_solar'})
+
+    #create new colum for month
+    df['month'] = df.index.month_name()
+    #create new colum for weekday
+    df['day_of_week'] = df.index.day_name()
+    #create new column for year
+    df['year'] = df.index.year
+    #fill nulls with 0
+    df = df.fillna(0)
+
+    return df
+
+
+# +
+########################## Prep Saas Data Function ##########################
+
+def prep_saas_data():
+    '''
+    This function takes in a df and changes date dtypes, resets date as index,
+    creates new columns for month, weekday, and total sales
+    and returns that as a new pandas dataframe
+    '''
+
+    #assign variable df from the acquire function
+    df= pd.read_csv('saas.csv')
+
+    #rename columns by lowercasing them all
+    df.columns = [col.lower() for col in df]
+
+    #change data type on Date
+    df.month_invoiced = df.month_invoiced.astype('datetime64[ns]')
+    #set the index to Date
+    df = df.set_index('month_invoiced').sort_index()
+    #change amount from float to int
+    df.amount = df.amount.astype(int)
+    #change sub type from float to int
+    df.subscription_type = df.subscription_type.astype(int)
+
+    #create new colum for month
+    df['month'] = df.index.month_name()
+    #create new colum for weekday
+    df['day_of_week'] = df.index.day_name()
+    #create new column for year
+    df['year'] = df.index.year
+
+    return df
+
+
+# +
+########################### Convert to Datetime Function #######################
+
+def convert_to_datetime(df):
+    '''
+    This function takes in a dataframe
+    and converts the sales_date column to a datetime
+    '''
+    df.sale_date = pd.to_datetime(df.sale_date, infer_datetime_format=True)
+    return df
+
+
+
+# +
+########################### Plot Distributions Function #########################
+
+def plot_distributions(df):
+    for col in list(df.columns.drop('Date')):
+        plt.figure()
+        sns.histplot(df[col])
+        plt.title('Distribution of {}'.format(col))
+# -
+
+
+
+
+
+# +
+#def prepare_store(df):
+#    """
+#    Prepares a sales DataFrame for analysis by:
+#    1. Converting sale dates to datetime format.
+#    2. Setting the datetime as the index.
+#    3. Extracting the month and day of the week.
+#    4. Calculating the total sales for each transaction.
+
+#    Args:
+#        df (DataFrame): The input sales DataFrame.
+
+#    Returns:
+#        DataFrame: The prepared DataFrame.
+#    """
+#    # Convert the sale date into a datetime
+#    df.sale_date = pd.to_datetime(df.sale_date, format='%Y-%m-%d')
+#    #df.sale_date = pd.to_datetime(df.sale_date, format='%a, %d %b %Y')
+#    # Set the index as the new datetime value
+#    df = df.set_index('sale_date')
+#    # Extract the month value (name of month)
+#    df['month'] = df.index.strftime('%B')
+#    # Extract the day of the week from the date
+#    df['day of the week'] = df.index.day_name()
+#    # Calculate the total sales
+#    df['sales_total'] = df.sale_amount * df.item_price
+#    return df
+
+# +
+#def prep_germany_data(df):
+#    '''
+#    Takes in a dataframe, coverts Date to DateTime format and makes it the index, produces histograms 
+#    of all variables, renames "Wind+Solar" to the more python-friendly "WindSolar", fills the abundant 
+#    nulls/NaNs with 0, and adds the columns 'month' and 'year'. We take a look at the amended dataframe
+#    and the dataframe is returned.
+#    '''
+#    # Convert date to datetime for time series analysis
+#    df.Date = pd.to_datetime(df.Date)
     
-    # Histogram of Consumption
-    df.Consumption.hist()
-    plt.title('Consumption')
-    plt.show()
+#    # Histogram of Consumption
+#    df.Consumption.hist()
+#    plt.title('Consumption')
+#    plt.show()
     
     # Histogram of Wind
-    df.Wind.hist()
-    plt.title('Wind')
-    plt.show()
+#    df.Wind.hist()
+#    plt.title('Wind')
+#    plt.show()
     
     # Histogram of Solar
-    df.Solar.hist()
-    plt.title('Solar')
-    plt.show()
+#    df.Solar.hist()
+#    plt.title('Solar')
+#    plt.show()
     
     # Histogram of Solar
-    df['Wind+Solar'].hist()
-    plt.title('WindSolar')
-    plt.show()
+#    df['Wind+Solar'].hist()
+#    plt.title('WindSolar')
+#    plt.show()
     
     # Rename column to python ok type
-    df = df.rename(columns={'Wind+Solar': 'WindSolar'})
+#    df = df.rename(columns={'Wind+Solar': 'WindSolar'})
     
     # Fill null values with 0
-    df = df.fillna(0)
+#    df = df.fillna(0)
     
     # Set date as index for time series analysis
-    df = df.set_index('Date').sort_index()
+#    df = df.set_index('Date').sort_index()
     
     # add columns to df
-    df['month'] = df.index.month
-    df['year'] = df.index.year
+#    df['month'] = df.index.month
+#    df['year'] = df.index.year
     
-    print(df.head())
+#    print(df.head())
     
-    return df
-
+#    return df
+# -
 
 def display_numeric_column_histograms(data_frame):
     """
@@ -113,7 +251,6 @@ def display_numeric_column_histograms(data_frame):
         sns.histplot(data_frame, x=column, ax=axis, color=colors[i % len(colors)])
         axis.set_title(f"Histogram of {column}")
         plt.show()
-
 
 
 def prepare_ops(df):
