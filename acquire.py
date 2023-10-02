ebdf3e2395cf6c0e9529907e7fdfd2cd1d4164cc
Aswathy Radha <aswathy.radha@gmail.com>
2023-10-02 09:13:08 -0500

    Acquire Functions

--- acquire.py
+++ acquire.py
@@ -12,12 +12,58 @@
 #     name: python3
 # ---
 
+# +
+# Import the Pandas library for data manipulation
 import pandas as pd
+
+# Import the NumPy library for numerical operations
 import numpy as np
+
+# Import the 'requests' library to make HTTP requests
 import requests
+
+# Import the 'os' library for operating system-related tasks
 import os
 
+# Import credentials from an 'env.py' file 
+from env import host, user, password
 
+# -
+
+def get_db_url(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+# +
+
+
+
+####################### grocery_df function  #########################
+
+
+def get_store_data():
+    '''
+    Returns a dataframe of all store data in the tsa_item_demand database and saves a local copy as a csv file.
+    '''
+    url = get_db_url('tsa_item_demand')
+
+    query = '''
+            SELECT *
+            FROM items
+            JOIN sales USING(item_id)
+            JOIN stores USING(store_id)
+            '''
+
+    df = pd.read_sql(query, url)
+    df.to_csv('tsa_store_data.csv', index=False)
+    return df
+
+
+# -
 def get_swapi_data(endpoint):
     """
     This function will:  
@@ -64,48 +110,5 @@ def opsd_germany_daily():
         df = pd.read_csv(url)
         df.to_csv('opsd_germany_daily.csv')
     return df
-
-
-# +
-####################### grocery_df function  #########################
-
-def get_store_data():
-    """
-    This function checks for csv files
-    for items, sales, stores, and big_df 
-    if there are none, it creates them.
-    It returns grocery_df of the merged dfs.
-    """
-    # check for csv files or create them
-    if os.path.isfile('items.csv'):
-        items_df = pd.read_csv('items.csv', index_col=0)
-    else:
-        items_df = get_df('items')
-        
-    if os.path.isfile('stores.csv'):
-        stores_df = pd.read_csv('stores.csv', index_col=0)
-    else:
-        stores_df = get_df('stores')
-        
-    if os.path.isfile('sales.csv'):
-        sales_df = pd.read_csv('sales.csv', index_col=0)
-    else:
-        sales_df = get_df('sales')
-        
-    if os.path.isfile('grocery_df.csv'):
-        df = pd.read_csv('grocery_df.csv', index_col=0)
-        return df
-    else:
-        # merge all of the DataFrames into one
-        df = pd.merge(sales_df, stores_df, left_on='store', right_on='store_id').drop(columns={'store'})
-        df = pd.merge(df, items_df, left_on='item', right_on='item_id').drop(columns={'item'})
-
-        # write merged df with all data to directory for future use
-        df.to_csv('grocery_df.csv')
-        return df
-    
-    
-
 # -
-
 
